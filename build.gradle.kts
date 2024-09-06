eaec3de4e9e7c07d4c1b089553a753db205a812f
EmptyIrony <1446188523@qq.com>
2024-09-06 08:34:33 +0800

    first init

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.izzel.taboolib.gradle.*
+import io.izzel.taboolib.gradle.BUKKIT_ALL
+import io.izzel.taboolib.gradle.UNIVERSAL
+import io.izzel.taboolib.gradle.DATABASE
+
+
+plugins {
+    java
+    id("io.izzel.taboolib") version "2.0.11"
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+}
+
+taboolib {
+    env {
+        install(BUKKIT_ALL)
+        install(UNIVERSAL)
+        install(DATABASE)
+    }
+    description {
+        name = "NewPlayerCode"
+        contributors {
+            name("存在")
+        }
+    }
+    version { taboolib = "6.1.1-beta17" }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("ink.ptms.core:v12004:12004:mapped")
+    compileOnly("ink.ptms.core:v12004:12004:universal")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
---
+++ src/main/kotlin/me/cunzai/plugin/newplayercode/NewPlayerCode.kt
@@ -0,0 +1,6 @@
+package me.cunzai.plugin.newplayercode
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object NewPlayerCode : Plugin()
